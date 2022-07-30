09b224ac6f2d12285ce28324698ddcd273cfa8da
EdiedRamos <edieisa22@gmail.com>
2022-07-30 00:16:18 -0500

    private/public routes implemented

--- src/Router/AppRouter.js
+++ src/Router/AppRouter.js
@@ -1,14 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { Login, Register, Dashboard } from "../Pages";
+
+const logged = false;
+
+const Private = ({ logged, children }) => {
+  return !logged ? <Navigate to="/login" replace /> : children;
+};
+
+const Public = ({ logged, children }) => {
+  return logged ? <Navigate to="/" replace /> : children;
+};
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/registro" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <Private logged={logged}>
+              <Dashboard />
+            </Private>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Public logged={logged}>
+              <Login />
+            </Public>
+          }
+        />
+        <Route
+          path="/registro"
+          element={
+            <Public logged={logged}>
+              <Register />
+            </Public>
+          }
+        />
         <Route path="*" element={<p>🔍Ruta no administrada🔎</p>} />
       </Routes>
     </BrowserRouter>
